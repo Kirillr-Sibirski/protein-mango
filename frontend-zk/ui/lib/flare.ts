89bddb6aa14eddffc2fbf28339a9d8e29e8edd40
ParanoidSyntaxError <git@jbojtor.com>
2025-02-08 20:25:05 +0000

    Setup fdc api call functions

--- frontend-zk/ui/lib/flare.ts
+++ frontend-zk/ui/lib/flare.ts
@@ -1,18 +1,31 @@
-export async function prepareFdcRequest(): Promise<string | undefined> {
+export async function prepareCoordsFdcRequest() {
+    
+}
+
+export async function prepareDisasterFdcRequest() {
+    
+}
+
+async function prepareFdcRequest(
+    attestationType: string,
+    sourceId: string,
+    url: string,
+    postprocessJq: string,
+    abiSignature: string
+): Promise<string | undefined> {
     try {
-        //https://jq-verifier-test.flare.rocks/
         const response = await fetch("https://jq-verifier-test.flare.rocks/JsonApi/prepareRequest", {
             method: "POST",
             headers: {
                 "X-API-KEY": "flare-oxford-2025"
             },
             body: JSON.stringify({
-                "attestationType": "0x494a736f6e417069000000000000000000000000000000000000000000000000",
-                "sourceId": "0x5745423200000000000000000000000000000000000000000000000000000000",
+                "attestationType": attestationType,
+                "sourceId": sourceId,
                 "requestBody": {
-                    "url": "https://jsonplaceholder.typicode.com/todos/1",
-                    "postprocessJq": ".",
-                    "abi_signature": "{\"components\": [{\"internalType\": \"uint8\",\"name\": \"userId\",\"type\": \"uint8\"},{\"internalType\": \"uint8\",\"name\": \"id\",\"type\": \"uint8\"},{\"internalType\": \"string\",\"name\": \"title\",\"type\": \"string\"},{\"internalType\": \"bool\",\"name\": \"completed\",\"type\": \"bool\"}],\"name\": \"task\",\"type\": \"tuple\"}"
+                    "url": url,
+                    "postprocessJq": postprocessJq,
+                    "abi_signature": abiSignature
                 }
             })
         });

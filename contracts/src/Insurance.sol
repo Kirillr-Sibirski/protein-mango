7ac3ef5638758fb0bf9fcff6c438755eb897495d
ParanoidSyntaxError <git@jbojtor.com>
2025-02-08 11:34:57 +0000

    Added events and insurance array getter

--- contracts/src/Insurance.sol
+++ contracts/src/Insurance.sol
@@ -4,49 +4,76 @@ pragma solidity ^0.8.27;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract InsuranceEscrow {
-    struct Vector2 {
-        uint256 x;
-        uint256 y;
-    }
+    event NewInsurance(
+        uint256 indexed id,
+        address indexed token,
+        uint256 indexed value
+    );
+
+    event PremiumPaid(uint256 indexed id, address indexed payer);
+
+    event ClaimPaid(
+        uint256 indexed id,
+        address indexed receiver,
+        uint256 indexed payout
+    );
 
     struct Insurance {
         address receiver;
         address token;
         uint256 premium;
         uint256 payout;
-        Vector2 location;
+        uint256 x;
+        uint256 y;
         uint256 radius;
         bytes32 snarkId;
-        mapping(address => uint256) timestamps;
+        uint256 value;
     }
 
-    mapping(uint256 => Insurance) private _insurances;
-    uint256 private _totalInsurances;
+    Insurance[] private _insurances;
+    // Insurance ID => Account => Timestamp
+    mapping(uint256 => mapping(address => uint256)) private _premiumTimestamps;
 
     constructor() {}
+
+    function getInsurances() external view returns (Insurance[] memory) {
+        return _insurances;
+    }
+
+    function getPremiumTimestamp(uint256 id, address account) external view returns (uint256) {
+        return _premiumTimestamps[id][account];
+    }
 
     function newInsurance(
         address receiver,
         address token,
         uint256 premium,
         uint256 payout,
-        Vector2 memory location,
+        uint256 x,
+        uint256 y,
         uint256 radius,
         bytes32 snarkId,
         uint256 value
     ) external returns (uint256 id) {
         IERC20(token).transferFrom(msg.sender, address(this), value);
 
-        id = _totalInsurances;
-        _totalInsurances++;
+        _insurances.push(
+            Insurance(
+                receiver,
+                token,
+                premium,
+                payout,
+                x,
+                y,
+                radius,
+                snarkId,
+                value
+            )
+        );
 
-        _insurances[id].receiver = receiver;
-        _insurances[id].token = token;
-        _insurances[id].premium = premium;
-        _insurances[id].payout = payout;
-        _insurances[id].location = location;
-        _insurances[id].radius = radius;
-        _insurances[id].snarkId = snarkId;
+        id = _insurances.length;
+
+        emit NewInsurance(id, token, value);
     }
 
     function payPremium(uint256 id) external {
@@ -56,18 +83,27 @@ contract InsuranceEscrow {
             _insurances[id].premium
         );
 
-        _insurances[id].timestamps[msg.sender] = block.timestamp;
+        _premiumTimestamps[id][msg.sender] = block.timestamp;
+
+        emit PremiumPaid(id, msg.sender);
     }
 
     function requestPayout(uint256 id) external {
+        if (block.timestamp - _premiumTimestamps[id][msg.sender] > 30 days) {
+            revert("");
+        }
+
         // TODO: Query disaster API
         // TODO: Query zkProof of location on Mina
+
+        _insurances[id].value -= _insurances[id].payout;
+        _premiumTimestamps[id][msg.sender] = 0;
 
         IERC20(_insurances[id].token).transfer(
             msg.sender,
             _insurances[id].payout
         );
 
-        _insurances[id].timestamps[msg.sender] = 0;
+        emit ClaimPaid(id, msg.sender, _insurances[id].payout);
     }
 }

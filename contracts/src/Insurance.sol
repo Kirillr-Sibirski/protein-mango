0e1309a38308ad192fb7e1d6aea988a9b9426736
ParanoidSyntaxError <git@jbojtor.com>
2025-02-08 00:15:41 +0000

    Initial insurance escrow contract

---
+++ contracts/src/Insurance.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract InsuranceEscrow {
+    struct Insurance {
+        address receiver;
+        address token;
+        uint256 premium;
+        uint256 payout;
+        bytes32 snarkId;
+        mapping(address => uint256) timestamps;
+    }
+
+    mapping(uint256 => Insurance) private _insurances;
+    uint256 private _totalInsurances;
+
+    constructor() {}
+
+    function newInsurance(
+        address receiver,
+        address token,
+        uint256 premium,
+        uint256 payout,
+        bytes32 snarkId,
+        uint256 value
+    ) external returns (uint256 id) {
+        IERC20(token).transferFrom(msg.sender, address(this), value);
+
+        id = _totalInsurances;
+        _totalInsurances++;
+
+        _insurances[id].receiver = receiver;
+        _insurances[id].token = token;
+        _insurances[id].premium = premium;
+        _insurances[id].payout = payout;
+        _insurances[id].snarkId = snarkId;
+    }
+
+    function payPremium(uint256 id) external {
+        IERC20(_insurances[id].token).transferFrom(
+            msg.sender,
+            _insurances[id].receiver,
+            _insurances[id].premium
+        );
+
+        _insurances[id].timestamps[msg.sender] = block.timestamp;
+    }
+
+    function requestPayout(uint256 id) external {
+        // TODO: Query disaster API
+        // TODO: Query zkProof of location on Mina
+
+        IERC20(_insurances[id].token).transfer(
+            msg.sender,
+            _insurances[id].payout
+        );
+
+        _insurances[id].timestamps[msg.sender] = 0;
+    }
+}
